a7d0a901d842385ae32d5e5ad9c40bb8ec700be7
Jeyson Mirabal <jeyson.mirabal@gmail.com>
2019-04-25 03:31:49 -0400

    recorder implanted

--- src/app/pages/record/record.page.ts
+++ src/app/pages/record/record.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { MediaCapture, CaptureVideoOptions, MediaFile } from '@ionic-native/media-capture/ngx';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { Storage } from '@ionic/storage';
+
+const MEDIA_FILES_KEY = 'mediaFiles';
 
 @Component({
   selector: 'app-record',
@@ -6,10 +13,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record.page.scss'],
 })
 export class RecordPage implements OnInit {
-
-  constructor() { }
+  mediaFiles = [];
+  @ViewChild('myvideo') myVideo: any;
+  
+  constructor(public navCtrl: NavController, private mediaCapture: MediaCapture, private storage: Storage, private file: File, private media: Media) {}
 
   ngOnInit() {
+    this.storage.get(MEDIA_FILES_KEY).then(res => {
+      this.mediaFiles = JSON.parse(res) || [];
+    })
+  }
+ 
+  ionViewDidLoad() {
+    
+  }
+ 
+  captureAudio() {
+    this.mediaCapture.captureAudio().then(res => {
+      this.storeMediaFiles(res);
+    }, (err: CaptureError) => console.error(err));
+  }
+ 
+  captureVideo() {
+    let options: CaptureVideoOptions = {
+      limit: 1,
+      duration: 30
+    }
+    this.mediaCapture.captureVideo(options).then((res: MediaFile[]) => {
+      let capturedFile = res[0];
+      let fileName = capturedFile.name;
+      let dir = capturedFile['localURL'].split('/');
+      dir.pop();
+      let fromDirectory = dir.join('/');      
+      var toDirectory = this.file.dataDirectory;
+      
+      this.file.copyFile(fromDirectory , fileName , toDirectory , fileName).then((res) => {
+        this.storeMediaFiles([{name: fileName, size: capturedFile.size}]);
+      },err => {
+        console.log('err: ', err);
+      });
+          },
+    (err: CaptureError) => console.error(err));
+  }
+ 
+  play(myFile) {
+    if (myFile.name.indexOf('.wav') > -1) {
+      const audioFile: MediaObject = this.media.create(myFile.localURL);
+      audioFile.play();
+    } else {
+      let path = this.file.dataDirectory + myFile.name;
+      let url = path.replace(/^file:\/\//, '');
+      let video = this.myVideo.nativeElement;
+      video.src = url;
+      video.play();
+    }
+  }
+ 
+  storeMediaFiles(files) {
+    this.storage.get(MEDIA_FILES_KEY).then(res => {
+      if (res) {
+        let arr = JSON.parse(res);
+        arr = arr.concat(files);
+        this.storage.set(MEDIA_FILES_KEY, JSON.stringify(arr));
+      } else {
+        this.storage.set(MEDIA_FILES_KEY, JSON.stringify(files))
+      }
+      this.mediaFiles = this.mediaFiles.concat(files);
+    })
+  }
+
+  buttonBack(){
+    this.navCtrl.navigateForward(`/tabs/tab1`);
   }
 
 }
